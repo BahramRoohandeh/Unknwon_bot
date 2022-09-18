afea39623d26d2514d14dfbf20fd61071fbbc448
Dakhesin <spring91113@gmail.com>
2022-09-18 21:28:02 +0430

    final template

--- src/bot.py
+++ src/bot.py
@@ -1,26 +1,6 @@
+
 import telebot
 import os
-from src.utiles.io import write_json
-from telebot import types
-from src.utiles.keyboards import create_keyboard, keyboards
-import emoji
 
 
-class Bot:
-	def __init__(self):
-		self.bot = telebot.TeleBot(os.environ["bot_token"] , parse_mode=None )
-		self.echo_all = self.bot.message_handler(func=lambda m: True)(self.echo_all)
-
-	def run(self):
-		self.bot.infinity_polling()
-
-	def echo_all(self, message):
-		#write_json(message.json , "message.json")
-		print(emoji.demojize(message.text))
-		self.bot.send_message(message.chat.id, str(message.text), reply_markup=keyboards.main)
-
-
-
-if __name__ == "__main__":
-	bot1 = Bot()
-	bot1.run()
+bot = telebot.TeleBot(os.environ["bot_token"] , parse_mode=None )
