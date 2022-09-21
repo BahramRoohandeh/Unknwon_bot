d034faf7907aaa3c8219b41824a2def7a4121e90
Dakhesin <spring91113@gmail.com>
2022-09-21 21:27:55 +0430

    adding message func

--- src/bot.py
+++ src/bot.py
@@ -3,4 +3,4 @@ import telebot
 import os
 
 
-bot = telebot.TeleBot(os.environ["bot_token"] , parse_mode=None )
+bot = telebot.TeleBot(os.environ["bot_token"] , parse_mode=None)
--- src/run.py
+++ src/run.py
@@ -1,12 +1,11 @@
-import telebot
-from src.utiles.io import write_json
-from telebot import types
-from src.utiles.keyboards import create_keyboard, keyboards
 import emoji
+import telebot
+from telebot import types
+
 from src.bot import bot
 from src.filters import IsAdmin
-
-
+from src.utiles.io import write_json
+from src.utiles.keyboards import create_keyboard, keyboards
 
 
 class Bot:
@@ -33,17 +32,27 @@ class Bot:
 
 		@self.bot.message_handler(is_admin=True)
 		def admin_of_group(message):
-			self.bot.send_message(message.chat.id, 'You are admin of this group!')
+			self.send_message(message.chat.id, 'You are admin of this group!')
 
 
 		@self.bot.message_handler(func=lambda m: True)
 		def echo_all(message):
 			#write_json(message.json , "message.json")
 			#print(emoji.demojize(message.text))
-			self.bot.send_message(message.chat.id, str(message.text), reply_markup=keyboards.main)
+			self.send_message(message.chat.id, str(message.text), reply_markup=keyboards.main)
 
+
+
+	def send_message(self, chat_id, text, reply_markup=None, emojize=True):
+
+		if emojize == True:
+			text = emoji.emojize(text)
+
+
+		self.bot.send_message(chat_id, text, reply_markup = reply_markup)
 
 
 if __name__ == "__main__":
 	bot1 = Bot(tele_bot=bot)
-	bot1.run()
+	#bot1.run()
+
